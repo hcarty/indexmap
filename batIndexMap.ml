c2c95f32044ddebed18ff04867787ca2372ddaef
Hezekiah M. Carty <hez@0ok.org>
2014-02-11 21:33:37 -0500

    Move library to src/; add _oasis

--- batIndexMap.ml
+++
@@ -1,6 +0,0 @@
-(* Examples of some data structures from Batteries *)
-let of_vect v =
-  IndexMap.make_ro (fun i -> BatVect.get v i)
-let of_map m =
-  IndexMap.make_ro (fun key -> BatMap.find key m)
-
--- batIndexMap.mli
+++
@@ -1,2 +0,0 @@
-val of_vect : 'e BatVect.t -> (int, 'e) IndexMap.ro
-val of_map : ('i, 'e) BatMap.t -> ('i, 'e) IndexMap.ro
